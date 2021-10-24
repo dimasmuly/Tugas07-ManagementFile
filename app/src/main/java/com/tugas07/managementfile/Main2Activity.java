d6e907609dc935a4e706eba1f04a2de3bdbd152f
dimasmuly <dimasgans39@gmail.com>
2021-10-24 17:27:17 +0700

    Tugas07

---
+++ app/src/main/java/com/tugas07/managementfile/Main2Activity.java
@@ -0,0 +1,76 @@
+package com.tugas07.managementfile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Main2Activity extends AppCompatActivity {
+
+    TextView showText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        showText = findViewById(R.id.txt_get);
+    }
+
+    public void getPublic(View view) {
+        File folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);   //folder name
+        File myFile = new File(folder, "myData1.txt");  //filename
+        String text = getData(myFile);
+        if (text != null) {
+            showText.setText(text);
+        } else {
+            showText.setText("No Data");
+        }
+    }
+
+    public void getPrivate(View view) {
+        File folder = getExternalFilesDir("tugas07");   //folder name
+        File myFile = new File(folder, "myData2.txt");  //filename
+        String text = getData(myFile);
+        if (text != null) {
+            showText.setText(text);
+        } else {
+            showText.setText("No Data");
+        }
+    }
+
+    public void back(View view) {
+        Intent intent = new Intent(Main2Activity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    private String getData(File myFile) {
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream(myFile);
+            int i = -1;
+            StringBuffer buffer = new StringBuffer();
+            while ((i = fileInputStream.read()) != -1) {
+                buffer.append((char) i);
+            }
+            return buffer.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (fileInputStream != null) {
+                try {
+                    fileInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+}
